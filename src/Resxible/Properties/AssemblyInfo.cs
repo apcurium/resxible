b293f41576bf6af8758abcff8bf1ef7e2cfa0002
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2016-05-20 08:47:27 -0400

    Bump v1.1.0.0

--- src/Resxible/Properties/AssemblyInfo.cs
+++ src/Resxible/Properties/AssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany ("Apcurium")]
 [assembly: AssemblyProduct ("")]
 [assembly: AssemblyCopyright("Copyright © 2015")]
-[assembly: AssemblyVersion ("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion ("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 [assembly: ComVisibleAttribute(false)]
